d88a35eb1d678fce8056089b6c28dc6a540e4bcc
Ben Matase <benjaminmatase@gmail.com>
2024-12-10 00:38:29 -0500

    Added welcome page

--- source/background.ts
+++ source/background.ts
@@ -1,2 +1,15 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage.js';
+import welcomeHtml from './welcome.html';
+
+const welcomeHtmlUrl = welcomeHtml as string;
+
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.tabs
+		.create({
+			url: welcomeHtmlUrl,
+		})
+		.catch((error: unknown) => {
+			console.error(error);
+		});
+});
